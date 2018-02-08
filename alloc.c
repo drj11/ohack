58a0f3e7d7d5da87cc2a714dbe01881db0070813
David Jones <drj@pobox.com>
2018-02-08 20:49:25 +0000

    modernise allocator

--- alloc.c
+++ alloc.c
@@ -1,34 +1,14 @@
-#ifdef LINT
-
-/*
-   a ridiculous definition, suppressing
-	"possible pointer alignment problem" for (long *) malloc()
-	"enlarg defined but never used"
-	"ftell defined (in <stdio.h>) but never used"
-   from lint
-*/
-#include <stdio.h>
-long *
-alloc(n) unsigned n; {
-long dummy = ftell(stderr);
-	if(n) dummy = 0;	/* make sure arg is used */
-	return(&dummy);
-}
-
-#else
-
-extern char *malloc();
-extern char *realloc();
+#include <stdlib.h>
 
 long *
 alloc(lth)
 register unsigned lth;
 {
-	register char *ptr;
+	register void *ptr;
 
 	if(!(ptr = malloc(lth)))
 		panic("Cannot get %d bytes", lth);
-	return((long *) ptr);
+	return(ptr);
 }
 
 long *
@@ -36,11 +16,10 @@ enlarge(ptr,lth)
 register char *ptr;
 register unsigned lth;
 {
-	register char *nptr;
+	register void *nptr;
 
 	if(!(nptr = realloc(ptr,lth)))
 		panic("Cannot reallocate %d bytes", lth);
-	return((long *) nptr);
+	return(nptr);
 }
 
-#endif
